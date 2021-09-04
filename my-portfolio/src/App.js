aa7f2753acfe464eeb979ce37e6aa1ba14e4c209
Adil Khimani <51865838+Adilcs@users.noreply.github.com>
2021-09-04 18:57:43 -0400

    React App Creation
    First commit

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <h1>
+          Website Creation (Initial Commit)
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
